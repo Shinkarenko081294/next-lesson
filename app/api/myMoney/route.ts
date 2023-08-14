536458010f87baec23ce389eed09f1b4632e7d6a
shinkar90 <asuborisov@gmail.com>
2023-08-14 16:34:29 +0300

    feat: added new user model, adedd api for user model, install mongoose, jwt, bcrypt and conecten MONGO DB

--- app/api/myMoney/route.ts
+++ app/api/myMoney/route.ts
@@ -1,13 +1,26 @@
 import {NextResponse} from "next/server";
+import {connectMongoDB} from "@/libs/mogodb";
+import User from "@/models/User";
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
 
-export async function GET(req: Request){
-   const CONNECT_DB = process.env["MONGO_CONNECT "]
-   const desc = "it test connect env file"
-   const users = await fetch(
-       `http://localhost:3000/api/posts`
-   )
+export async function POST(req: Request){
+   const {email,password } = await req.json()
+   await connectMongoDB()
+   const user = await User.findOne({email: email})
+   if(!user){
+      console.log('this email not found!!')
+   }
+   const isValidPass = await bcrypt.compare(password, user._doc.passwordHash)
+   if(!isValidPass){
+      console.log('error not validate')
+   }
+   const token = jwt.sign({
+      _id: user._id,
+   }, 'secret123',{
+      expiresIn: '30d',
+   })
+   const {passwordHash, ...userData} = user._doc;
 
-   console.log(CONNECT_DB)
-
-   return NextResponse.json({CONNECT_DB, desc})
+   return NextResponse.json({...userData, token})
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,8 @@
 export default function Home() {
   return (
-    <h1>Welcome to netx js</h1>
+      <>
+        <h1>Welcome to netx js</h1>
+        <a href={'https://www.youtube.com/watch?v=wNWyMsrpbz0&ab_channel=GTCoding'}>Link lesson Mongo from Next</a>
+      </>
   )
 }
